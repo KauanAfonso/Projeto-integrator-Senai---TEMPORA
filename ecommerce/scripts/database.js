383540164153c2776d18f79eb396834b80c39ee3
Kauan <144381805+KauanAfonso@users.noreply.github.com>
2024-12-09 07:42:13 -0300

    Add files via upload

---
+++ ecommerce/scripts/database.js
@@ -0,0 +1,278 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto: 1,
+        tituloProduto: "Rolex Oyster Perpetual 36mm com Data",
+        preco: 8999.99,
+        descricao: "Um modelo refinado que mistura sofisticação e precisão, ideal para quem busca um relógio elegante e versátil.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-1.jpg?w=1500&h=1500&v=no-value",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-2.jpg?w=1500&h=1500&v=no-value",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-4.jpg?w=1500&h=1500&v=no-value",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-3.jpg?w=1500&h=1500&v=no-value",
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 2,
+        tituloProduto: "Rolex Datejust 31mm - Clássico e Elegante",
+        preco: 7999.99,
+        descricao: "Design intemporal e ideal para o uso diário, com a qualidade inconfundível da Rolex.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-3.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-2.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-6.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-4.jpg?w=1500&h=1500&v=202411080939",
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 3,
+        tituloProduto: "Omega Seamaster 300m - Profissional",
+        preco: 10500.99,
+        descricao: "Desenvolvido para mergulhadores e aventureiros, este relógio é resistente à água e possui uma estética refinada.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-3.jpg?w=1500&h=1500&v=202411081509",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-2.jpg?w=1500&h=1500&v=202411081509",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-5.jpg?w=1500&h=1500&v=202411081509",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-6.jpg?w=1500&h=1500&v=202411081509",
+        },
+        categoriaProduto: "Esportivo",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 4,
+        tituloProduto: "Rolex Datejust 31mm Edição Limitada",
+        preco: 11500.75,
+        descricao: "Edição especial da Rolex, um modelo único que reflete tradição e estilo moderno.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-5.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-2.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-3.jpg?w=1500&h=1500&v=202411080939",
+            img5: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-1.jpg?w=1500&h=1500&v=202411080939",
+            
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 5,
+        tituloProduto: "Omega Seamaster Co-Axial - Mergulho Profundo",
+        preco: 12500.89,
+        descricao: "Relógio projetado para aventuras subaquáticas com alta tecnologia e resistência, oferecendo precisão excepcional.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-3.jpg?w=1500&h=1500&v=202411081511",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-4.jpg?w=1500&h=1500&v=202411081511",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-2.jpg?w=1500&h=1500&v=202411081511",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-5.jpg?w=1500&h=1500&v=202411081511",
+        },
+        categoriaProduto: "Esportivo",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 6,
+        tituloProduto: "Montblanc Sport GMT - Aventura e Estilo",
+        preco: 4700.50,
+        descricao: "Perfeito para quem combina praticidade e elegância em um relógio esportivo.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-montblanc-sport-gmt-210295/411592-2.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-montblanc-sport-gmt-210295/411592-5.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-montblanc-sport-gmt-210295/411592-1.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-montblanc-sport-gmt-210295/411592-4.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Promoções",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 7,
+        tituloProduto: "Relógio Rolex Oyster Perpetual Datejust 36mm",
+        preco: 9500.75,
+        descricao: "Este Rolex icônico traz uma combinação de tradição e inovação. O Datejust 36mm é perfeito para quem busca um relógio casual com precisão e durabilidade inigualáveis.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-2.jpg?w=1500&h=1500&v=no-value",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-1.jpg?w=1500&h=1500&v=no-value",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-4.jpg?w=1500&h=1500&v=no-value",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-oyster-perpetual-datejust-36mm-209679/410951-3.jpg?w=1500&h=1500&v=no-value"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 8,
+        tituloProduto: "Relógio Rolex Datejust 31mm",
+        preco: 9700.89,
+        descricao: "O Rolex Datejust 31mm oferece um design compacto e refinado, ideal para quem aprecia um relógio esportivo de alta classe e com excelente precisão.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-2.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-6.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-3.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-4.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Esportivo",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 9,
+        tituloProduto: "Relógio Omega Seamaster Professional 300",
+        preco: 8900.99,
+        descricao: "O Omega Seamaster Professional 300 é a escolha perfeita para aventureiros que buscam um relógio esportivo, confiável e com design sofisticado.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-2.jpg?w=1500&h=1500&v=202411081509",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-3.jpg?w=1500&h=1500&v=202411081509",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-5.jpg?w=1500&h=1500&v=202411081509",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-6.jpg?w=1500&h=1500&v=202411081509"
+        },
+        categoriaProduto: "Esportivo",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 10,
+        tituloProduto: "Relógio Rolex Oyster Perpetual DateJust 38mm",
+        preco: 2800.99,
+        descricao: "O Rolex Oyster Perpetual Datejust 38mm é uma peça emblemática que combina a tradição da relojoaria suíça com inovações técnicas, sendo um dos modelos mais icônicos e reconhecíveis da marca. Este relógio é conhecido por sua elegância atemporal e por ser um exemplo perfeito de precisão e durabilidade.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-3.jpg?w=1500&h=1500&v=202411081509",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-2.jpg?w=1500&h=1500&v=202411081509",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-5.jpg?w=1500&h=1500&v=202411081509",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-6.jpg?w=1500&h=1500&v=202411081509"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 11,
+        tituloProduto: "Relógio Rolex Oyster Perpetual Datejust 36mm",
+        preco: 8330.99,
+        descricao: "O Rolex Oyster Perpetual Datejust 36mm é uma peça emblemática que combina a tradição da relojoaria suíça com inovações técnicas, sendo um dos modelos mais icônicos e reconhecíveis da marca. Este relógio é conhecido por sua elegância atemporal e por ser um exemplo perfeito de precisão e durabilidade.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-3.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-2.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-1.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-5.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 12,
+        tituloProduto: "Relógio Rolex Oyster Perpetual Datejust 36mm",
+        preco: 8550.99,
+        descricao: "O Rolex Oyster Perpetual Datejust 36mm é uma peça emblemática que combina a tradição da relojoaria suíça com inovações técnicas, sendo um dos modelos mais icônicos e reconhecíveis da marca. Este relógio é conhecido por sua elegância atemporal e por ser um exemplo perfeito de precisão e durabilidade.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-3.jpg?w=1500&h=1500&v=202411081511",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-4.jpg?w=1500&h=1500&v=202411081511",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-2.jpg?w=1500&h=1500&v=202411081511",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-co-axial-210297/411594-5.jpg?w=1500&h=1500&v=202411081511"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 13,
+        tituloProduto: "Relógio Rolex Oyster Perpetual Datejust 36mm",
+        preco: 12999.99,
+        descricao: "O Rolex Oyster Perpetual Datejust 36mm é uma peça emblemática que combina a tradição da relojoaria suíça com inovações técnicas, sendo um dos modelos mais icônicos e reconhecíveis da marca. Este relógio é conhecido por sua elegância atemporal e por ser um exemplo perfeito de precisão e durabilidade.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-professional-300-210299/411595-3.jpg?w=1500&h=1500&v=202411081509",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-professional-300-210299/411595-2.jpg?w=1500&h=1500&v=202411081509",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-professional-300-210299/411595-5.jpg?w=1500&h=1500&v=202411081509",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seamaster-professional-300-210299/411595-4.jpg?w=1500&h=1500&v=202411081509"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto: 14,
+        tituloProduto: "Relógio Omega Seamaster Diver 300M 36mm",
+        preco: 12000.99,
+        descricao: "O Omega Seamaster Diver 300M 36mm é a escolha perfeita para quem busca um relógio de luxo com robustez e estilo incomparáveis. Com design inovador e resistência à água de até 300 metros, este relógio é ideal para os amantes de esportes aquáticos e para quem aprecia a tradição da relojoaria suíça.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-2.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-6.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-3.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210298/411595-4.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 15,
+        tituloProduto: "Relógio Omega Seamaster Diver 300M 36mm",
+        preco: 8000.99,
+        descricao: "Este relógio Omega Seamaster Diver 300M 36mm é um ícone de resistência e sofisticação. Perfeito para os entusiastas do mergulho e também para quem aprecia um design moderno e elegante. Sua caixa de aço inoxidável e o movimento de alta precisão tornam-no um dos relógios mais desejados.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-2.jpg?w=1500&h=1500&v=202411081509",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-3.jpg?w=1500&h=1500&v=202411081509",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-5.jpg?w=1500&h=1500&v=202411081509",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-omega-seeamaster-professional-300-210296/411593-6.jpg?w=1500&h=1500&v=202411081509"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 16,
+        tituloProduto: "Relógio Tag Heuer Aquaracer 36mm",
+        preco: 10500.99,
+        descricao: "O Tag Heuer Aquaracer 36mm é um relógio de alta performance que alia resistência e design esportivo. Perfeito para os aficionados por esportes náuticos e quem busca um relógio versátil e sofisticado. Com um sistema de fechamento inovador e um estilo incomparável, ele é ideal para aventuras e para o uso diário.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-2.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-3.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-1.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-rolex-datejust-31mm-210299/411596-5.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 17,
+        tituloProduto: "Relógio Tag Heuer Monaco 36mm",
+        preco: 9200.99,
+        descricao: "O Tag Heuer Monaco 36mm é um modelo clássico e único que transmite sofisticação e ousadia. Sua caixa quadrada e o movimento preciso fazem dele uma peça de destaque no mundo dos relógios de luxo. Ideal para ocasiões especiais e para quem aprecia design inovador e alta performance.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-tag-heuer-monaco-36mm-210299/411597-1.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-tag-heuer-monaco-36mm-210299/411597-2.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-tag-heuer-monaco-36mm-210299/411597-3.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-tag-heuer-monaco-36mm-210299/411597-4.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 18,
+        tituloProduto: "Relógio Breitling Superocean 36mm",
+        preco: 9800.99,
+        descricao: "O Breitling Superocean 36mm é um relógio robusto, feito para os aventureiros e para os entusiastas do mergulho. Sua caixa de aço inoxidável e resistência à água de até 500 metros fazem dele o modelo perfeito para quem precisa de confiabilidade e resistência em condições extremas.",
+        imagemProduto: {
+            img1: "https://orit.fbitsstatic.net/img/p/relogio-breitling-superocean-36mm-210300/411598-2.jpg?w=1500&h=1500&v=202411080939",
+            img2: "https://orit.fbitsstatic.net/img/p/relogio-breitling-superocean-36mm-210300/411598-1.jpg?w=1500&h=1500&v=202411080939",
+            img3: "https://orit.fbitsstatic.net/img/p/relogio-breitling-superocean-36mm-210300/411598-3.jpg?w=1500&h=1500&v=202411080939",
+            img4: "https://orit.fbitsstatic.net/img/p/relogio-breitling-superocean-36mm-210300/411598-4.jpg?w=1500&h=1500&v=202411080939"
+        },
+        categoriaProduto: "Casual",
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    }
+    
+    
+
+
+]
---
+++ https_server.py
@@ -0,0 +1,27 @@
+import http.server
+import socketserver
+import ssl
+
+# Configurações do servidor
+PORT = 8080
+DIRECTORY = "./ecommerce"  # Diretório onde os arquivos estão
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler): 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'  # Página padrão
+        return super().do_GET()
+
+# Inicia o servidor com SSL
+with socketserver.ThreadingTCPServer(("", PORT), MyHttpRequestHandler) as httpd:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+
+    # Envolvendo o socket do servidor com SSL
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+    print(f"Servidor HTTPS iniciado na porta {PORT}. Acesse https://localhost:{PORT}")
+    httpd.serve_forever()
