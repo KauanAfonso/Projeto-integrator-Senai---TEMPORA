a5a71e7b83fd4cd5f650ca577c2505f02249d39d
kauanAfonso <kauanafon3@gmail.com>
2024-12-13 10:40:40 -0300

    Ajustando o pdf

--- ecommerce/manipulacao_arquivo/index.py
+++ ecommerce/manipulacao_arquivo/index.py
@@ -2,7 +2,7 @@ import pandas as pd
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
-caminho = "planilha_esteira.csv"
+caminho = "planilha_esteira.csv" #caminho do arquivp
 
 arquivo_csv = pd.read_csv(caminho)
 print(arquivo_csv)
@@ -12,43 +12,54 @@ arquivo_csv.to_excel("Esteira.xlsx", index=False)
 arquivo_csv.to_json("Esteira.json", index=False)
 
 #criando o pdf
+canva = canvas.Canvas("esteiras.pdf", pagesize=letter)
+
+#desenhando o pdf(capa)
+canva.drawString(x=240, y=700, text="SENAI ROBERTO MANGE")
+canva.drawString(x=240, y=680, text="KAUAN AFONSO DA SILVA")
+canva.drawString(x=125, y=421, text="PDF DE PRODUTOS NA ESTEIRA DA TEMPORA PARA DESPACHE")
+
+canva.drawString(x=270, y=135, text="CAMPINAS")
+canva.drawString(x=290, y=115, text="2024")
+canva.showPage()
+
+#definindo coordenadas para iteração
 x = 120
 y = 720
 
 
-
-canva = canvas.Canvas("esteiras.pdf", pagesize=letter)
-canva.drawString(x=120, y=750, text="PDF DE PRODUTOS NA ESTEIRA DA TEMPORA PARA DESPACHE")
-canva.drawString(x, y, text=f"-------------------------------------------------------------------")
-
-
-
-
 for i in arquivo_csv.iterrows():
 
-    largura, altura = letter  # Pega as dimensões da página
+    #------------------------- bordas--------------------------
+    largura, altura = letter  
 
     # Adicionando uma borda ao redor da página
     espessura_borda = 2  
-    # Desenhando a borda 
+
     canva.setStrokeColorRGB(0, 0, 0)  
     canva.setLineWidth(espessura_borda)  
     canva.rect(0, 0, largura, altura)  
+    #------------------------Fim das bordas--------------------------
 
+
+    #Conteudo do csv para a planilha
     linha = i[1]
+
     esteira1 = linha.Esteira1
     esteira2 = linha.Esteira2
     esteira3 = linha.Esteira3
+
     data = linha.Data
 
     y-=20
 
     canva.drawString(x, y, text=f"O valor da esteira 1 é {esteira1} na data {data}")
     y-=20
+
     canva.drawString(x, y, text=f"O valor da esteira 2 é {esteira2} na data {data}")
     y-=20
+
     canva.drawString(x, y, text=f"O valor da esteira 3 é {esteira3} na data {data}")
-    # x -=1
     y-=20
 
     canva.drawString(x, y, text=f"-------------------------------------------------------------------")
@@ -59,6 +70,4 @@ for i in arquivo_csv.iterrows():
         canva.showPage()
         y = 720
     
-    
-
 canva.save()
